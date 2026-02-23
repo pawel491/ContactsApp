8bf06570058b0b6b4391e3c9c04623e7092cce47
pawel491 <s197977@student.pg.edu.pl>
2026-02-23 15:10:51 +0100

    feat: seed initial data and add db migration

--- backend/ContactsApp/Data/AppDbContext.cs
+++ backend/ContactsApp/Data/AppDbContext.cs
@@ -16,14 +16,26 @@ public class AppDbContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
 
+        modelBuilder.Entity<Category>()
+            .HasKey(c => c.Id);
+        modelBuilder.Entity<Category>()
+            .HasData(
+                new Category { Id = 1, Name = "służbowy"},
+                new Category { Id = 2, Name = "prywatny"},
+                new Category { Id = 3, Name = "inny" }
+            );
+
         modelBuilder.Entity<Contact>()
             .HasKey(c => c.Id);
         modelBuilder.Entity<Contact>()
             .HasIndex(c => c.Email)
             .IsUnique();
+        modelBuilder.Entity<Contact>()
+            .HasData(
+                new Contact { Id = 1, Email = "test@aa.aa", Name = "Test", Surname = "Testowy", Password = "somepassword", PhoneNumber = "+48123123123", DateOfBirth = new DateOnly(2000,1,1), CategoryId = 1},
+                new Contact { Id = 2, Email = "testtest@aa.aa", Name = "TestDrugi", Surname = "TestowyDwa", Password = "somepasswordlonger", PhoneNumber = "+48999999999", DateOfBirth = new DateOnly(2005,3,3), CategoryId = 2}
+            );
 
-        modelBuilder.Entity<Category>()
-            .HasKey(c => c.Id);
 
     }
 }
