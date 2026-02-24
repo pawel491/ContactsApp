110a1e1262a4af4937608ccbf182ef5849ee8cad
pawel491 <s197977@student.pg.edu.pl>
2026-02-24 17:11:32 +0100

    feat: add cors policy so frontend is allowed to fetch data

--- backend/ContactsApp/Program.cs
+++ backend/ContactsApp/Program.cs
@@ -3,9 +3,24 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var frontendUrl = builder.Configuration.GetValue<string>("Frontend:Url");
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var AllowFrontendOrigins = "_allowFrontendOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowFrontendOrigins,
+    policy =>
+    {
+        policy.WithOrigins(frontendUrl)
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
@@ -15,6 +30,8 @@ using (var scope = app.Services.CreateScope())
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate();
 }
+
+app.UseCors(AllowFrontendOrigins);
 
 app.MapControllers();
 app.Run();
