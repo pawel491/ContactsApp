a8e8ebfab7da0f48eeb8b4006d3a009129ff098e
pawel491 <s197977@student.pg.edu.pl>
2026-02-25 15:20:21 +0100

    feat: add phone and email annotations to validate those fields

--- backend/ContactsApp/Domain/Dto/CreateContactDto.cs
+++ backend/ContactsApp/Domain/Dto/CreateContactDto.cs
@@ -5,9 +5,11 @@ namespace ContactsApp.Domain.Dto;
 public record class CreateContactDto(
     string Name, 
     string Surname, 
+    [EmailAddress]
     string Email, 
     [MinLength(8, ErrorMessage = "The password must be at least 8 characters long.")]
     string Password, 
+    [Phone]
     string PhoneNumber, 
     DateOnly DateOfBirth, 
     string CategoryName,
--- backend/ContactsApp/Domain/Dto/UpdateContactDto.cs
+++ backend/ContactsApp/Domain/Dto/UpdateContactDto.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ContactsApp.Domain.Dto;
 
 public record class UpdateContactDto(
     string Name, 
     string Surname, 
+    [EmailAddress]
     string Email, 
+    [Phone]
     string PhoneNumber,
     DateOnly DateOfBirth, 
     string CategoryName,
